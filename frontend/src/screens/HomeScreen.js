795c14b8bb6e2dffc849791452ddc9c1cb0af56a
Mario Vizcaino <vizhar1952@gmail.com>
2021-01-23 22:46:33 -0500

    added useDispatch and useSelector into homescreen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,31 +1,35 @@
-import React, {useState, useEffect} from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Dish from '../components/Dish'
-import axios from 'axios'
+import { listDishes } from '../actions/dishActions'
 
 
 const HomeScreen = () => {
-    const [dishes, setDishes] = useState([])
+    const dispatch = useDispatch()
+
+    const dishList = useSelector(state => state.dishList)
+    const { loading, error, dishes } = dishList
 
     useEffect(() => {
-    const fetchDishes = async () => {
-        const { data } = await axios.get('/api/dishes')
-
-        setDishes(data)
-    }
-        fetchDishes()
-    }, [])
+        dispatch(listDishes())
+    }, [dispatch])
 
     return (
         <>
             <h1>Best Dishes</h1>
-            <Row>
-                {dishes.map(dish => (
-                    <Col key={dish._id} sm={12} md={6} lg={4} xl={3}>
-                    <Dish dish={dish}/>
-                    </Col>
-                ))}
-            </Row>
+            {loading ? 
+                <h2>Loading...</h2> 
+                : error ? 
+                <h3>{error}</h3> :             
+                <Row>
+                    {dishes.map(dish => (
+                        <Col key={dish._id} sm={12} md={6} lg={4} xl={3}>
+                        <Dish dish={dish}/>
+                        </Col>
+                    ))}
+                </Row>}
+
         </>
     )
 }
