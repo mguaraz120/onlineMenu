0c45875d2a51b88cc14faa4947bc685bb9042f4b
Mario Vizcaino <vizhar1952@gmail.com>
2021-01-24 20:34:21 -0500

    ceated the add to cart funcionality

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { Row, Col, Listgroup, Image, Form, Button, Card } from 'react-bootstrap'
+import Message from '../components/Message'
+import { addToCart } from '../actions/cartActions'
 
-const CartScreen = () => {
+
+const CartScreen = ( {match, location, history} ) => {
+    const DishId = match.params.id
+
+    const qty = location.search ? Number(location.search.split('=')[1]) : 1
+
+    const dispatch = useDispatch()
+
+    const cart = useSelector(state => state.cart)
+    const { cartItems } = cart
+
+    useEffect(() => {
+        if (DishId) {
+            dispatch(addToCart(DishId, qty))
+        }
+    }, [dispatch, DishId, qty])
+
     return (
         <div>
             Cart
