94f373beb35cf8b364d932ff1762c873fc3d52fa
Mario Vizcaino <vizhar1952@gmail.com>
2021-01-31 23:52:42 -0500

    created edit screen and get user details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import PaymentScreen from './screens/PaymentScreen'
 import PlaceOrderScreen from './screens/PlaceOrderScreen'
 import OrderScreen from './screens/OrderScreen'
 import UsersListScreen from './screens/UsersListScreen'
+import UserEditScreen from './screens/UserEditScreen'
 
 function App() {
   return (
@@ -29,6 +30,7 @@ function App() {
           <Route path='/dish/:id' component={DishScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
           <Route path='/admin/userlist' component={UsersListScreen} />
+          <Route path='/admin/user/:id/edit' component={UserEditScreen} />
           <Route path='/' component={HomeScreen} exact />
         </Container>
       </main>
